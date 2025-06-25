e2db08772d116b6384bb902576d4e1bf25899c13
oladapo.aiyenitaju <dapssyola@gmail.com>
2025-06-25 15:01:59 +0300

    Add homework 9

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -47,7 +47,7 @@ test('post order with correct data should receive code 201', async ({ request })
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
-test('post order with Incorrect data should receive code 201', async ({ request }) => {
+test('post order with Incorrect data should receive code 400', async ({ request }) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN1',
@@ -65,4 +65,98 @@ test('post order with Incorrect data should receive code 201', async ({ request 
   console.log('response status:', response.status())
   //console.log('response body:', await response.text())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+//PUT REQUEST
+
+test('PUT order with correct data and API key should receive 200', async ({ request }) => {
+  // Define headers with API key
+  const requestHeaders: { api_key: string } = {
+    api_key: '1234567890123456', // Replace with your actual key
+  }
+  // Prepare JSON request body
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+
+  const response = await request.put(baseURL+'/10', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+
+  // Log and verify
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+//Put Negative request
+
+test('PUT order with incorrect data and API key should receive 400', async ({ request }) => {
+  // Define headers with API key
+  const requestHeaders: { api_key: string } = {
+    api_key: '1234567890123456', // Replace with your actual key
+  }
+  // Prepare JSON request body
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+
+  const response = await request.put(baseURL+'/18', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+
+  // Log and verify
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+// DEL REQUEST
+
+test('Delect order with correct data and API key should receive 204', async ({ request }) => {
+  // Define headers with API key
+  const requestHeaders: { api_key: string } = {
+    api_key: '1234567890123456', // Replace with your actual key
+  }
+  // Prepare JSON request body
+
+  const response = await request.delete(baseURL+'/10', {
+    headers: requestHeaders,
+  })
+
+  // Log and verify
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+
+  expect(response.status()).toBe(204)
+})
+
+test('Delect order with Incorrect data and API key should receive 400', async ({ request }) => {
+  // Define headers with API key
+  const requestHeaders: { api_key: string } = {
+    api_key: '1234567890123456', // Replace with your actual key
+  }
+  // Prepare JSON request body
+
+  const response = await request.delete(baseURL+'/120', {
+    headers: requestHeaders,
+  })
+
+  // Log and verify
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+
+  expect(response.status()).toBe(400)
 })
