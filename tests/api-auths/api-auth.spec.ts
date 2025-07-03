1e7ceb90eed91708c20568238ba70345ba86d70a
oladapo.aiyenitaju <dapssyola@gmail.com>
2025-07-03 23:48:05 +0300

    Homework 11 first commit

---
+++ tests/api-auths/api-auth.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test'
+import { LoginDto } from '../homeWorkDto/login-dto'
+import { StatusCodes } from 'http-status-codes'
+
+const authURL = 'https://backend.tallinn-learning.ee/login/student'
+
+// GET REQUEST
+test('Login to a student with incorrect credentials', async ({ request }) => {
+  const loginData = new LoginDto(' string123', 'string123')
+  const apiResponse = await request.post(authURL, {
+    data: loginData,
+  })
+  expect.soft(apiResponse.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('Login to a student with correct credentials', async ({ request }) => {
+  const loginData = LoginDto.createLoginWithCorrectData()
+  const response = await request.post(authURL, {
+    data: loginData,
+  })
+
+  const responseBody = await response.text()
+  console.log('response body:', responseBody)
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(responseBody).toBeDefined()
+})
